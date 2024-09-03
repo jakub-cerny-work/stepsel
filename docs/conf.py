eb43a19da29c08c821e6c0b2a29d71049416db56
Jakub Cerny <jakubcerny@email.com>
2024-09-03 21:32:34 +0200

    try: adjustments to render documentation on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+# The name of the jupyter kernel to use for the notebooks
+nbsphinx_kernel_name = "stepsel-py312"
+
 ## https://stackoverflow.com/questions/5599254/how-to-use-sphinxs-autodoc-to-document-a-classs-init-self-method
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
